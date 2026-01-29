2950973b16d2eedb7ab44472dfcac8b626ccbf12
dngsondev <dngsondev@gmail.com>
2026-01-29 16:24:12 +0700

    init backend

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -47,7 +47,6 @@ const connection = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
